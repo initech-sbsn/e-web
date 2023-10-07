dee7f0cf2257550f84464866d59e1c3f4374ad11
Sebastian Bauersfeld <seb@gwp2vhh690.fritz.box>
2023-10-07 08:24:42 +0200

    update

--- webapp-legacy/src/main/java/com/example/springboot/Application.java
+++ webapp-legacy/src/main/java/com/example/springboot/Application.java
@@ -6,15 +6,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-import java.io.IOException;
-
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) throws IOException {
-		String a = args[0];
-		Runtime.getRuntime().exec(a);
-
+	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(Application.class, args);
 
 		System.out.println("Beans provided by Spring Boot:");
